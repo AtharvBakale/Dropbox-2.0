781cb515216d82c379b32a1725e343d3acd352f8
Atharv Bakale <164789324+AtharvBakale@users.noreply.github.com>
2024-03-28 13:40:14 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -5,12 +5,7 @@ import { getFunctions } from "firebase/functions";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3FFf9QbLWU6G3S0-SH9GzHCwORFAeL_I",
-  authDomain: "dropbox-clone-eef62.firebaseapp.com",
-  projectId: "dropbox-clone-eef62",
-  storageBucket: "dropbox-clone-eef62.appspot.com",
-  messagingSenderId: "118266600914",
-  appId: "1:118266600914:web:a1b6df0ebcbf5aa2017c0b",
+  PASTE YOUR CONFIG HERE
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
